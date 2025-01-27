5a395bcc0e3193e0ac76daed235fd9afff04cc7a
haton14 <haton14@haton14.com>
2025-01-27 10:21:50 +0900

    コマンドライン引数から元金、金利、期間を受け取る機能を追加

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	loango "github.com/haton14/loan-go"
@@ -8,14 +9,23 @@ import (
 )
 
 func main() {
-	principal, err := udecimal.NewFromUint64(8000*10000, 0)
+	var (
+		principalVar    int64
+		interestRateVar float64
+		termVar         int
+	)
+	flag.Int64Var(&principalVar, "principal", 1000*10000, "元金 - 借りる金額 (単位: 円)")
+	flag.Float64Var(&interestRateVar, "interestRate", 1.0, "金利(年利)(%)")
+	flag.IntVar(&termVar, "term", 35*12, "期間(月)")
+	flag.Parse()
+	principal, err := udecimal.NewFromInt64(principalVar, 0)
 	if err != nil {
 		panic(err)
 	}
-	interestRate, err := udecimal.NewFromFloat64(0.625)
+	interestRate, err := udecimal.NewFromFloat64(interestRateVar)
 	if err != nil {
 		panic(err)
 	}
-	result := loango.CalculateMonthlyPayment(principal, interestRate, 40*12)
+	result := loango.CalculateMonthlyPayment(principal, interestRate, loango.Term(termVar))
 	fmt.Println(result)
 }
